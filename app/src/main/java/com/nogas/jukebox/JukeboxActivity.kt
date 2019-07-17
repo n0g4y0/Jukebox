10d31150e9a2209ded2fe8e2e085c986f5974eb6
n0g4y0 <n0g4y0@gmail.com>
2019-07-17 15:59:31 -0400

    la version 1 de los SERVICES, lista

--- app/src/main/java/com/nogas/jukebox/JukeboxActivity.kt
+++ app/src/main/java/com/nogas/jukebox/JukeboxActivity.kt
@@ -1,5 +1,6 @@
 package com.nogas.jukebox
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -8,36 +9,50 @@ import kotlinx.android.synthetic.main.activity_jukebox.*
 
 class JukeboxActivity : AppCompatActivity() {
 
-    private lateinit var songs: Array<String>           // list of all songs
-    private var media : MediaPlayer? = null             // media player for current song
-    private var index = -1                              // current song index being played
+
 
     //  Initializes the activity when it is created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jukebox)
 
-        songs = resources.getStringArray(R.array.song_titles)
+
 
         song_list.setOnItemClickListener { _, _, index, _ ->
-            playSong(index)
+            playSongAtIndex(index)
         }
     }
 
-    ///// Event - Listener methods
+    private fun doIntent(action:String){
+        val intent = Intent(this,JukeboxService::class.java)
+        intent.action = action
+        startService(intent)
+    }
+
+    fun playSongAtIndex(index:Int){
+        val intent = Intent(this,JukeboxService::class.java)
+        intent.action = JukeboxService.PLAY_INDEX_ACTION
+        intent.putExtra("index",index)
+        startService(intent)
+
+    }
 
     //  plays the song at the current index
     fun onCLickPlay(view:View){
-        playSong(index)
+        //playSong(index)
+        // tell the service to play the song
+        doIntent(JukeboxService.PLAY_ACTION)
     }
 
     //  goes to the next track
     fun onClickNextTrack(view:View){
-        playNextSong()
+        //playNextSong()
+        doIntent(JukeboxService.NEXT_TRACK_ACTION)
     }
 
     fun onClickStop(view:View){
-        stopSong()
+        //stopSong()
+        doIntent(JukeboxService.STOP_ACTION)
     }
 
 
@@ -47,43 +62,4 @@ class JukeboxActivity : AppCompatActivity() {
     //Plays the track at the given index.
     //Stop playing any other track that was playing.
     //Also sets this index to be the new current index.
-
-    private fun playSong(index:Int){
-        stopSong()
-
-        // get resource ID, example: "E Honda" -> R.raw.ehonda
-        this.index = index % songs.size
-        val songName = songs[this.index].toLowerCase().replace(" ","")
-        val resID = resources.getIdentifier(songName,"raw",packageName)
-        if (resID <= 0) return
-
-        media = MediaPlayer.create(this,resID)
-        if (media == null)  return
-        media?.setOnCompletionListener {
-            onSongComplete()
-        }
-        media?.start()
-
-    }
-
-    //  Moves to the next track, then starts playing it
-    private fun playNextSong(){
-        index = (index +1) % songs.size
-        playSong(index)
-    }
-
-    private fun onSongComplete(){
-        //  go to the next song, if still playing
-        if (media != null){
-            playNextSong()
-        }
-    }
-
-    //  stop playing any song that is currently playing.
-    private fun stopSong(){
-        if (media !=null){
-            media?.stop()
-            media = null
-        }
-    }
 }
